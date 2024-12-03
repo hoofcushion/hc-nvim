881f59f35ee7e925171e2ce827d94231028bd341
Hoofcushion <hoofcushion@qq.com>
2024-12-03 19:56:59 +0800

    chore: clean unused file

--- lua/hc-nvim/util/vscode_neovim.lua
+++
@@ -1,6 +0,0 @@
-local Config=require("lazy.core.config")
-Config.options.checker.enabled=false
-Config.options.change_detection.enabled=false
-Config.options.defaults.cond=function(plugin)
- return plugin.vscode==nil or plugin.vscode==true
-end
