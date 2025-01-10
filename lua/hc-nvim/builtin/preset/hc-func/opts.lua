5f19caed97c6692cff1ca8cf8d448e565dd48178
hoofcushion <2465403648@qq.com>
2025-01-10 19:42:39 +0800

    fix: call table in hc-func opts.lua

--- lua/hc-nvim/builtin/preset/hc-func/opts.lua
+++ lua/hc-nvim/builtin/preset/hc-func/opts.lua
@@ -1,4 +1,10 @@
-return {
+return (function(opts)
+ for k,v in pairs(opts) do
+  if k~="toggler" and v.enabled~=false then
+   v.enabled=vim.g.vscode==nil
+  end
+ end
+end)({
  rainbowcursor={
   throttle=5,
   autocmd={
@@ -13,7 +19,7 @@ return {
  auto_format={enabled=false},
  inlay_hint={enabled=false},
  semantic_tokens={enabled=false},
- document_highlight={enabled=vim.g.vscode==nil},
+ document_highlight={enabled=true},
  toggler={
   enabled=true,
   rule={
@@ -30,4 +36,4 @@ return {
    },
   },
  },
-}
+})
