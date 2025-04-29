c61b2d0499f8b0d939920fc5ff5f7906feb37268
hoofcushion <2465403648@qq.com>
2025-04-30 04:22:59 +0800

    fix: fn.has(...) *== 1*

--- lua/hc-nvim/builtin/preset/mason/base.lua
+++ lua/hc-nvim/builtin/preset/mason/base.lua
@@ -9,7 +9,7 @@ return {
   "MasonUninstallAll",
  },
  init=function()
-  local sep=vim.fn.has("win32") and ";" or ":"
+  local sep=vim.fn.has("win32")==1 and ";" or ":"
   vim.env.PATH=vim.env.PATH..sep..vim.fs.joinpath(vim.fn.stdpath("data"),"mason","bin")
  end,
 }
