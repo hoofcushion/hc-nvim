f7c2ae33edd9eb3fb23db4f2554bc15fd2faf330
hoofcushion <2465403648@qq.com>
2025-05-27 20:57:43 +0800

    update

--- lua/hc-coordinate/function.lua
+++ lua/hc-coordinate/function.lua
@@ -1,51 +1,102 @@
 local Config=require("hc-coordinate.config")
 local M={}
-function M.fini()
+function M.fini() end
+local function utf8_offset(str,nth)
+ local max=vim.fn.strcharlen(str)+1
+ nth=math.min(math.max(1,nth),max)
+ return vim.fn.byteidx(str,nth-1)+1
+end
+local function utf8_charpos(str,nth)
+ local s=utf8_offset(str,nth)
+ local e=utf8_offset(str,nth+1)
+ return s,e
+end
+local function utf8sub(str,nth)
+ local s,e=utf8_charpos(str,nth)
+ return str:sub(s,e-1)
+end
+local function get_absolute_col(lnum,vcol)
+ local line=vim.fn.getline(lnum)
+ local charidx=1
+ local cur_vcol=1
+ local byte=0
+ local offset=0
+ while cur_vcol<vcol do
+  local char=utf8sub(line,charidx)
+  if char=="" then
+   break
+  end
+  local width=vim.fn.strdisplaywidth(char)
+  if cur_vcol+width>vcol then
+   offset=vcol-cur_vcol
+   break
+  end
+  cur_vcol=cur_vcol+width
+  charidx=charidx+1
+  byte=byte+#char
+ end
+ local extra_byte=0
+ while cur_vcol<vcol do
+  cur_vcol=cur_vcol+1
+  extra_byte=extra_byte+1
+ end
+ return {
+  vcol=vcol,
+  pos=byte,
+  extra_pos=extra_byte,
+  char_offset=offset,
+ }
 end
 local labels=vim.split("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()","")
 local function init_grid()
  local cursor_pos=vim.api.nvim_win_get_cursor(0)
- local row,col=cursor_pos[1],cursor_pos[2]
- local row0=row-1
+ local row,col=cursor_pos[1]-1,cursor_pos[2]
  local ns_id=vim.api.nvim_create_namespace("coordinate_system")
  vim.api.nvim_buf_clear_namespace(0,ns_id,0,-1)
- local range=1
- vim.api.nvim_buf_set_extmark(0,ns_id,row0,col,{
-  virt_text={{"0","Comment"}},
-  virt_text_pos="overlay",
-  hl_mode="combine",
- })
+ local range=#labels
  local vcol=vim.fn.virtcol(".")
  local line_count=vim.api.nvim_buf_line_count(0)
- local pos0=vim.fn.screenpos(0,row,col)
  -- for i=-range,range do
  for i=-range,range do
   if i~=0 then
-   local row_y=row0+i
-   if row_y>1 and row_y<=line_count then
-    local vcol_y=vim.fn.virtcol2col(0,row0,vcol)-1
-    local pos_y=vim.fn.screenpos(0,row0,vcol_y)
-     vim.api.nvim_buf_set_extmark(0,ns_id,row_y,vcol_y,{
-      priority=1000, -- 汉汉汉汉汉汉
-      strict=false,
-      virt_text_pos="overlay",
-      virt_text={
-       {(" "):rep(0)},
-       {labels[math.abs(i)],"Substitute"},
-      },
-     })
+   local row_y=row+i
+   local lnum_y=row+i+1
+   if lnum_y>=1 and lnum_y<=line_count then
+    local pos=get_absolute_col(lnum_y,vcol)
+    vim.api.nvim_buf_set_extmark(0,ns_id,row_y,pos.pos,{
+     priority=1000,
+     strict=false,
+     virt_text_pos="overlay",
+     virt_text={
+      {(" "):rep(pos.extra_pos)},
+      {(" "):rep(-pos.char_offset)},
+      {labels[(math.abs(i)-1)%#labels+1],"Substitute"},
+     },
+    })
    end
-   local len_x=#vim.fn.getline(row0+1)
-   local col_x=col+i
-   if col_x>=0 and col_x<=len_x then
-    vim.api.nvim_buf_set_extmark(0,ns_id,row0,col_x,{
-     virt_text={{labels[math.abs(i)],"Substitute"}},
+   local len_x=#(vim.fn.getline(row+1))
+   local posx=get_absolute_col(row+1,vcol+i)
+   if col+i>=0 and col+i<=len_x then
+    vim.api.nvim_buf_set_extmark(0,ns_id,row,posx.pos,{
+     virt_text={
+      {labels[(math.abs(i)-1)%#labels+1],"Substitute"},
+     },
      virt_text_pos="overlay",
      hl_mode="combine",
     })
    end
   end
  end
+ local _pos=get_absolute_col(row+1,vcol)
+ vim.api.nvim_buf_set_extmark(0,ns_id,row,_pos.pos,{
+  virt_text={
+   {(" "):rep(_pos.extra_pos)},
+   {(" "):rep(-_pos.char_offset)},
+   {"0",                        "Substitute"},
+  },
+  virt_text_pos="overlay",
+  hl_mode="combine",
+ })
 end
 function close()
  local ns_id=vim.api.nvim_create_namespace("coordinate_system")
